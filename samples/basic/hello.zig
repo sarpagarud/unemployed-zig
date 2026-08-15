67e255c1d39d5fed85beb75a196aad1ad36e2546
sarpagarud <241446179+sarpagarud@users.noreply.github.com>
2026-08-15 17:00:31 +0530

    Update hello.zig

--- samples/basic/hello.zig
+++ samples/basic/hello.zig
@@ -4,6 +4,7 @@ const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
     try std.Io.File.stdout().writeStreamingAll(init.io, "Hello, World!\n");
+    std.debug.print("Hello, {s}!\n", .{"World"});
 }
 
 // exe=succeed
