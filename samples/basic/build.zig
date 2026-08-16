25fb425e44691d6f9231cb925e9ee3685ee24a73
sarpagarud <241446179+sarpagarud@users.noreply.github.com>
2026-08-16 17:41:25 +0530

    Update build.zig to include target options and install
    Added target options and installation for the executable.

--- samples/basic/build.zig
+++ samples/basic/build.zig
@@ -1,15 +1,17 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
         .name = "hello",
         .root_module = b.createModule(.{
             .root_source_file = b.path("hello.zig"),
+            .target = target,
             .optimize = optimize,
         }),
     });
-    b.default_step.dependOn(&exe.step);
+    b.installArtifact(exe);
 }
 
 // syntax
