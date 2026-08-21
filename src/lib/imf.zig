9e0dfee7a8592264015ad8ef8753b4567c4b5605
sarpagarud <241446179+sarpagarud@users.noreply.github.com>
2026-08-21 18:21:34 +0530

    Update imf.zig

--- src/lib/imf.zig
+++ src/lib/imf.zig
@@ -1,1 +1,9 @@
 // IMF
+const csv = @import("csv.zig")
+
+pub fn get_imf_data(
+  io: std.Io,
+  allocator: std.mem.Allocator
+) void {
+  const csv_data = csv.get_csv_data(io, allocator, "../data/weoall.csv");
+}
