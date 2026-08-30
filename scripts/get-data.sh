84277ceff11b63d9661b09a08f912eab8814a01c
sarpagarud <241446179+sarpagarud@users.noreply.github.com>
2026-08-31 00:26:40 +0530

    Implement date subtraction for API request
    Added a function to subtract years from the current date and updated the API call to use the calculated date.

--- scripts/get-data.sh
+++ scripts/get-data.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
+
+subtract_years() {
+    local date="$1"
+    local years="$2"
+    date -d "$date -$years years" +%Y-%m-%d
+}
+
+DATE=$(date +%Y-%m-%d)
+YEARS=5
+RESULT=$(subtract_years "$DATE" "$YEARS")
+
 wget --header="Accept: text/csv" \
   -O ../src/data/weo.csv \
-  'https://api.imf.org/external/sdmx/3.0/data/dataflow/IMF.RES/WEO/9.0.0/%2A.%2A.%2A?c%5BTIME_PERIOD%5D=ge:2026-01-01'
+  "https://api.imf.org/external/sdmx/3.0/data/dataflow/IMF.RES/WEO/9.0.0/%2A.%2A.%2A?c%5BTIME_PERIOD%5D=ge:$RESULT"
 
 
 wget --header="Accept: text/csv" \
